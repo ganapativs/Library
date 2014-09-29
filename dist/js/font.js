fd0cf83855c9eff1a119da7578450e72e3741471
GuNs <guns.mylife@gmail.com>
2014-09-29 15:59:09 +0530

    font seperated

---
+++ dist/js/font.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Ganapati on 29-09-2014.
+ */
+WebFontConfig = {
+    google: { families: [ 'Roboto:400,300,700,500:latin' ] }
+};
+(function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
