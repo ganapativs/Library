3baceccd9b39f9cc07397dc262396e6f2bc87be1
GuNs <guns.mylife@gmail.com>
2014-09-29 19:08:58 +0530

    Final search page

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,3 +14,16 @@ $('.filterContents').bind('click',function(){
 
    $(this).next().toggle();
 });
+
+(function () {
+    'use strict';
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        var msViewportStyle = document.createElement('style')
+        msViewportStyle.appendChild(
+            document.createTextNode(
+                '@-ms-viewport{width:auto!important}'
+            )
+        )
+        document.querySelector('head').appendChild(msViewportStyle)
+    }
+})();
